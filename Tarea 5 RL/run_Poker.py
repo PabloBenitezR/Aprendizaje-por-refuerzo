a540e474950c49207e8f6e427f8e09011eb5061f
PabloBenitezR <pablo.ben.037@gmail.com>
2025-05-07 23:30:14 -0600

    Update run_Poker.py

--- Tarea 5 RL/run_Poker.py
+++ Tarea 5 RL/run_Poker.py
@@ -1,3 +1,4 @@
+import numpy as np
 from Poker_env import Poker
 
 env = Poker()
@@ -6,7 +7,6 @@ step = 0
 match_done = False
 
 while not match_done:
-   
     print(f"\n--- ronda {step} ---")
     env.render()  
     action = env.action_space.sample()
@@ -15,34 +15,33 @@ while not match_done:
     obs, reward, done_hand, truncated, info = env.step(action)
     print("Recompensa recibida:", reward)
 
-    step += 1
+    step +=1
 
-   
+    if np.isscalar(reward):        
+        gain = reward
+    else:                          
+        gain = reward[0]
+
     if done_hand:
-        if reward > 0:
+        
+        if gain > 0:
             print("Ganador de la mano: Agente")
-        elif reward < 0:
+        elif gain < 0:
             print("Ganador de la mano: Oponente")
         else:
             print("Empate en la mano")
-        print(f"Fin de mano →Stacks: {env.stacks}\n")
+        print(f"Fin de mano Stacks: {env.stacks}\n")
 
         
-        if env.stacks[1] <= 0:
-            print("\nOponente sin fichas, river directo")
-            hole_agent   = [env._int2card(c) for c in env.hands[0]]
-            hole_opponent= [env._int2card(c) for c in env.hands[1]]
-            community    = [env._int2card(c) for c in env.community]
-            print("Hole Agent    :", hole_agent)
-            print("Hole Opponent :", hole_opponent)
-            print("Community     :", community)
+        if env.stacks[1] == 0:
+            print("\nOponente sin fichas")
             match_done = True
         
         elif env.stacks[0] >= env.small_blind and env.stacks[1] >= env.big_blind:
             obs, _ = env._reset_hand()
 
         else:
-            print("\nfinde la partida")
+            print("\nfin de la partida")
             print("¡Se acabaron las fichas!")
             print("Stacks finales:", env.stacks)
-            match_over = True
+            match_done = True
